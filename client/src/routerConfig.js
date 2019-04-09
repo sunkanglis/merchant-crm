3505a095de641d0efcc856b005f567df8f7645c9
孙康力 <sunkl@knowbox.cn>
2019-04-09 16:01:10 +0800

    first

---
+++ client/src/routerConfig.js
@@ -0,0 +1,61 @@
+// 以下文件格式为描述路由的协议格式
+// 你可以调整 routerConfig 里的内容
+// 变量名 routerConfig 为 iceworks 检测关键字，请不要修改名称
+
+import UserLogin from './pages/UserLogin';
+import UserRegister from './pages/UserRegister';
+import Dashboard from './pages/Dashboard';
+import Reserve from './pages/Reserve';
+import Asset from './pages/Asset';
+import OrderList from './pages/OrderList';
+import Goods from './pages/Goods';
+import Membership from './pages/Membership';
+import AddReserve from './pages/AddReserve';
+
+import BasicLayout from './layouts/BasicLayout';
+import AddGoods from './pages/AddGoods';
+
+const routerConfig = [
+  {
+    path: '/goods',
+    component: Goods,
+  },
+  {
+    path: '/user/login',
+    component: UserLogin,
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '/reserve',
+    component: Reserve,
+  },
+  {
+    path: '/asset',
+    component: Asset,
+  },
+  {
+    path: '/user/register',
+    component: UserRegister,
+  },
+  {
+    path: '/order',
+    component: OrderList,
+  },
+  {
+    path: '/membership',
+    component: Membership,
+  },
+  {
+    path: '/add/reserve',
+    component: AddReserve,
+  },
+  {
+    path: '/add/goods',
+    component: AddGoods,
+  },
+];
+
+export default routerConfig;
